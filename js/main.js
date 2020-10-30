5498cb206b369a5985d3a63954562962a09fb304
pavedom13 <pavedom13@gmail.com>
2020-10-30 17:02:43 +0300

    горизонтальное выпадающее меню

--- js/main.js
+++ js/main.js
@@ -1,8 +1,18 @@
-$(function(){
+$(function () {
     $('.slider').slick({
-   arrows: false,
-   fade: true,
-   autoplay: 3000,
-   dots: true
+        arrows: false,
+        fade: true,
+        autoplay: 3000,
+        dots: true
     });
+
+    $('.header-btn').on('click', function(){
+        $('.menu').addClass('active');
+
+    });
+    $('.close-btn').on('click', function(){
+        $('.menu').removeClass('active');
+
+    });
+   
 });
